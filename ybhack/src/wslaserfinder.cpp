dcf24e5302fc7383fc70725282da00735105449d
praman2s <praveen.ramanujam@yahoo.com>
2011-11-29 12:39:25 +0100

    Ravi commits soon

---
+++ ybhack/src/wslaserfinder.cpp
@@ -0,0 +1,17 @@
+/* youbot Hackathon ----
+ * The goal of this class is to find the start , goal and temporay position
+ * in solving tower of hanoi problem . One of the method will return the
+ * the (x,y,theta) 
+ * Base navigation and partly arm navigation will use these retrun values */
+
+#include<>   //Ravi includes the libraries required .. structure pkgname/xyz.h
+
+
+
+//Do not forget to update the header file wslaserfinder.h
+namespace aanpr
+{
+	class wslaserfinder
+	{
+	};
+}
